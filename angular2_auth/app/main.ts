652975d45b1a56f7b294ee95bfd1583b82f412b0
liguanghao <chiansoft_lgh@outlook.com>
2017-03-28 21:33:20 +0800

    33

--- angular2_auth/app/main.ts
+++
@@ -1,7 +0,0 @@
-/**
- * Created by china on 2017/2/12.
- */
-import {platformBrowserDynamic} from '@angular/platform-browser-dynamic';
-import {AppModule} from './app.module';
-const platform = platformBrowserDynamic();
-platform.bootstrapModule(AppModule);
